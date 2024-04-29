31645438f822602c4d1132aebb44524d1a23f393
KathirG123 <165251477+KathirG123@users.noreply.github.com>
2024-04-29 16:02:25 +0530

    Add files via upload

---
+++ WEBFRAMEWORK  CW/Day12/Day12/ex1/src/main/java/com/example/ce1/controller/kathirEmployeeController.java
@@ -0,0 +1,36 @@
+package com.example.ce1.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.ce1.model.DpakEmployee;
+import com.example.ce1.service.DpakEmployeeService;
+
+@RestController
+public class DpakEmployeeController {
+    @Autowired
+    DpakEmployeeService employeeService;
+
+    @PostMapping("/employee")
+    public DpakEmployee setMethod(@RequestBody DpakEmployee employee)
+    {
+        return employeeService.setEmployee(employee);
+    }
+
+    @GetMapping("/employees-inner-join")
+    public List<DpakEmployee> getInnerMethod()
+    {
+        return employeeService.getInnerEmployee();
+    }
+
+    @GetMapping("/employees-left-outer-join")
+    public List<DpakEmployee> getLeftMethod()
+    {
+        return employeeService.getLeftOuterEmployee();
+    }
+}
